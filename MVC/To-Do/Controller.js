6d9065cb80da5818ef47118c912e8f82d969c991
robertomorano <roberto.morano@iesnervion.es>
2025-03-18 23:10:43 +0100

    Update Controller.js

--- MVC/To-Do/Controller.js
+++ MVC/To-Do/Controller.js
@@ -1,14 +1,20 @@
 class TareasController{
     constructor(){
-        this.tareas = new TareasModel();
+        this.modelo = new TareasModel();
         this.vista = new TareasView();
 
         this.vista.botonAñadir(this.botonAñadirControl.bind(this));
+        this.view.accionEliminar(this.eliminarTareas,bind(this))
     }
     botonAñadirControl(){
        let tarea = this.vista.cogerInput();
-       this.tareas.añadirTarea(tarea);
+       this.modelo.añadirTarea(tarea);
        this.vista.limpiarInput();
-       this.vista.renderTareas(this.tareas.Tareas);
+       this.vista.renderTareas(this.modelo.Tareas);
+    }
+
+    eliminarTareas(index){
+        this.modelo.eliminar(index);
+        this.actualizarVista();
     }
 }
